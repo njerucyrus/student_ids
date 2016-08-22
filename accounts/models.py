94e0ed330651676caa7a7877e1c1695d6e151a2c
= <njerucyrus5@gmail.com>
2016-08-22 23:42:24 +0300

    added .gitignore

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    phoneNumber = models.CharField(max_length=13)
+    regNo = models.CharField("RegNo", max_length=32, unique=True)
+    national_id = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.regNo
+
