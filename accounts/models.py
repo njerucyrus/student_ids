4db2a13b5575fd1a1ddf41d9bf417621873b92c4
= <njerucyrus5@gmail.com>
2016-08-22 23:56:09 +0300

    created models

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,18 @@ class Profile(models.Model):
     phoneNumber = models.CharField(max_length=13)
     regNo = models.CharField("RegNo", max_length=32, unique=True)
     national_id = models.PositiveIntegerField()
+    school = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.regNo
+
+
+class IdApplication(models.Model):
+    user = models.OneToOneField(User)
+    regNo = models.CharField(max_length=32)
+    full_name = models.CharField(max_length=100)
+    passport = models.ImageField(upload_to="img/passports")
 
     def __unicode__(self):
         return self.regNo
