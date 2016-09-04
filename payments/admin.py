b26e41a923cfc0d70ef3bbc207de424e4bb9a077
= <njerucyrus5@gmail.com>
2016-09-04 09:49:29 +0300

    created payment model

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from payments.models import Payment
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['transaction_id',
+                    'phoneNumber',
+                    'regNo',
+                    'amount',
+                    'status',
+                    'transaction_date'
+                    ]
+
+    class Meta:
+        model = Payment
+admin.site.register(Payment, PaymentAdmin)
--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    transaction_id = models.CharField(max_length=128)
+    phoneNumber = models.CharField(max_length=13)
+    regNo = models.CharField(max_length=32)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=32)
+    transaction_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.phoneNumber
+
+
